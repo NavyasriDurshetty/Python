b748a50a31c521ad4f95d7f1fd6016b55cee7af8
NavyasriDurshetty <durshettynavya@gmail.com>
2025-08-18 19:51:43 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,17 @@
+#print numbers from 100 to 0 in reverse which are divisible by 5
+'''for i in range(100,1,-1):
+    if(i%5==0):
+        print(f"{i}")'''
+        
+#check whether given string is a palindrome or  not using a function
+'''def is_palindrome(str):
+  for i in range(len(str)-2,0,-1):
+      if str:
+          print("it is palindrome")
+      else:
+          print("not a palindrome") 
+is_palindrome("lol")    '''       
+
+#wirte a function to print tables in reverse
+'''for x in range(10,0,-1):
+    print(f"3x{x}={3*x}")'''
