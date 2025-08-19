930146c9ffa039f41840cbddc7f627bd1fe86f59
NavyasriDurshetty <durshettynavya@gmail.com>
2025-08-19 14:15:09 +0530

    Add files via upload

---
+++ looptask.py
@@ -0,0 +1,42 @@
+'''l1=["sravan","harish","aravind","akhil"]
+l2=[24,17,20,18]
+for i in range(len(l1)):
+    if l2[i]>=18:
+        print(f"{l1[i]}-{l2[i]}-eligible")
+    else:
+        print(f"{l1[i]}-{l2[i]}-not eligible")'''
+        
+
+'''n=123
+res=0
+while n>0:
+    r=n%10
+    res+=r
+    n=n//10
+print(res)        
+
+n=120
+res=0
+while n>0:
+    r=n%10
+    res+=r
+    n=n//10
+print(res) '''
+
+'''n=123498
+res=-1
+while n>0:
+    r=n%10
+    if r>=res:
+        res=r
+        n=n//10
+print(res)''' 
+
+'''a=21
+c=a
+w=a
+while w>=3:
+    extra_choc=w//3
+    c+=extra_choc
+    w=extra_choc+w%3
+print(c) '''
