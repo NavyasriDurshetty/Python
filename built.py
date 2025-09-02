104e116e3163edba632f54c1cad47e0b6448b223
NavyasriDurshetty <durshettynavya@gmail.com>
2025-09-02 16:03:57 +0530

    Add files via upload

---
+++ built.py
@@ -0,0 +1,74 @@
+#number methods
+#absolute value
+'''a=-3
+print(abs(a))'''
+
+#int 
+'''a=-8
+print(abs(int(a)))'''
+
+#round function
+'''num=17.823918317
+print(round(num,3))'''
+
+#float 
+'''a=4
+print(float(a))'''
+
+#divmod method
+'''num=234
+print(divmod(num,10))'''
+
+#complex method
+'''a=4
+img_val=3
+print(complex(a,img_val))'''
+
+#type
+'''num=[10,10.0,11,11.0,12,13]
+def sample():
+    for i in num:
+        print(isinstance(i,int))
+sample()   '''     
+
+#math modules
+#factorial
+'''import math
+math.factorial(5)
+print(math.factorial(5))'''
+
+#ceil
+'''import math
+num=5.7
+print(math.ceil(num))'''
+
+#trunc
+'''import math
+num=5.7
+print(math.trunc(num))'''
+
+#floor
+'''import math
+num=-5.7
+print(math.floor(num))'''
+
+#fabs
+'''import math
+num=-5
+print(math.fabs(num))'''
+
+#lcm
+'''import math
+num1=45
+num2=70
+print(math.lcm(num1,num2))'''
+
+#gcd
+'''import math
+num1=45
+num2=70
+print(math.gcd(num1,num2))'''
+
+#power
+'''import math
+print(math.pow(25,2))'''
