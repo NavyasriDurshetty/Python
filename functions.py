ede392bc55107b85ad20924ee6f794d2074159c9
NavyasriDurshetty <durshettynavya@gmail.com>
2025-09-08 17:21:46 +0530

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,92 +1,36 @@
-'''def fruits():
-    print("mango","apple")
-    return "fruits are healthy"
-fruits()'''
-
+#swapcase
 '''def sample():
-    print("hi")
-sample()'''
+    s="NaVyA"
+    print(s.swapcase())
+sample()  '''  
 
-'''def Dynamic_fun(name):
-    print(f"hello{name}")
-Dynamic_fun("navya")
-Dynamic_fun("sony") '''  
+'''#strip()
+def strip():
+    s="   hii  "
+    print(s.strip())
+strip()   ''' 
 
-'''def veggies(vegetable, cost):
-    print(vegetable)
-    print(cost)
-veggies(vegetable="palak",cost=20)
-veggies(vegetable="radish",cost=30)'''
+#lstrip
+'''def lstrip():
+    s="   hloo   "
+    print(s.lstrip())
+lstrip()  '''  
 
-'''def student(name,rollno):
-    print(f"hello my name is{name},my rollno is{rollno}")
-student("navya",502)'''
-
-'''def add(*args):
-    print(sum(args))
-add(5,3,6,78,2,8)'''
-
-'''def add(*args):
-    result="  ".join(args)
-    print(result)
-add("hello","hii","monkey")'''
-
-'''def details(**a):
-    print(a)
-details(name="navya",pin="507",place="hyd")'''
-
-'''def student_details(name,rollno,batch="67r"):
-    print(f"hello iam{name},my rollno is{rollno}and batch is{batch}")
-student_details("navya",534)'''
-    
-#lambda function
-'''x=lambda :print("hello world")
-x()'''
-
-'''x=lambda:"hello"
-print(x())'''
-
-'''x=lambda a:a+4
-print(x(2))'''
-
-'''x=lambda a,b:a+b
-print(x(4,5))'''
-
-'''def add(a,b):
-    print(a+b)
-add(5,6)'''
-
-'''def check_even_odd(num):
-    if num %2==0:
-        print("The num is even")
-    else:
-        print("The num is odd") 
-check_even_odd(2)'''   
+#rstrip
+'''def rstrip():
+    s="   hello  "
+    print(s.rstrip())
+rstrip()    '''
 
 
-'''def check_leap_year(year):
-    if (year %4 ==0):
-        if( year %100!=0 or year %400 ==0 ):
-           print("its leap year")
-    else:
-           print("not a leap year") 
-check_leap_year(2020)'''
-           
-           
-'''a=5
-b=8
-if(a>b):
-    print("a is greater")
-else:
-    print("b is greater")'''
-    
-'''x=17
-if x%2==0 and x%3==0 and x%6==0:   
-    print("x is div by 2,3,6")   
-else:                 
-     print("not divisible")'''      
-           
-           
-           
-        
-                       
+#count
+'''def count():
+    s="ababgsa"
+    print(s.count("a"))
+count()    '''
+
+#replace
+'''def replace():
+    s="madam"
+    print(s.replace("m","b"))
+replace() '''   
