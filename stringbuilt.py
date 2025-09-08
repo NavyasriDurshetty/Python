a1642a21f6fca56328dee1682b2d94f06982888a
NavyasriDurshetty <durshettynavya@gmail.com>
2025-09-08 18:56:01 +0530

    Add files via upload

---
+++ stringbuilt.py
@@ -0,0 +1,49 @@
+#STRING  BUILT-IN FUNCTIONS:
+'''1.	casefold()
+Python String casefold() method is used to convert string to lowercase. It is similar to the Python lower() string method, but the case removes all the case distinctions present in a string.
+
+Syntax:
+s.casefold()
+
+
+2.    center()
+ center() method in Python is a simple and efficient way to center-align strings within           a given width. By default, it uses spaces to fill the extra space but can also be customized to     use any character we want.
+              Synatx:
+              s.center()
+              
+              
+3.encode()
+String encode() method in Python is used to convert a string into bytes using a specified encoding format.
+                 Syntax:
+                 s.encode()
+                 
+                 
+4.expandtabs()
+expandtabs() method in Python is used to replace all tab characters (\t) in a string with spaces. This method allows for customizable spacing, as we can specify the number of spaces for each tab. It is especially useful when formatting text for better readability or alignment.
+                  Syntax:
+                  s.expandtabs()
+                  
+                  
+5.replace()
+The replace() method replaces all occurrences of a specified substring in a string and returns a new string without modifying the original string.
+                    Syntax:
+                     s.replace()
+                     
+                     
+6.format()
+Formats strings with placeholders.
+                    Syntax: s.format()'''
+                    
+          
+          
+#write internal code for isalnum and find functions
+'''def isalnum(): 
+    a="a3b2c4"
+    print(a.isalnum())
+isalnum()    '''
+
+'''def find():
+    s="aravind"
+    print(s.find("v"))
+find()'''    
+             
